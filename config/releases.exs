b3d93445c4f6e565eb830bc77137d583c26aba2b
Amin Arria <arria.amin@gmail.com>
2020-12-21 00:22:28 -0300

    Use SSL for database connection in prod environment

--- config/releases.exs
+++ config/releases.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :newsletter_sponsorships, NewsletterSponsorships.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
