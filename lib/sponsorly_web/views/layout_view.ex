5781a0123101735d6cc0ca79a6a8a2f39fc3e267
Amin Arria <arria.amin@gmail.com>
2021-01-09 02:33:56 -0300

    Bulma flash messages

--- lib/sponsorly_web/views/layout_view.ex
+++ lib/sponsorly_web/views/layout_view.ex
@@ -1,3 +1,25 @@
 defmodule SponsorlyWeb.LayoutView do
   use SponsorlyWeb, :view
+
+  def flash(nil, _) do
+    nil
+  end
+
+  def flash(content, :info) do
+    ~E"""
+    <div class="notification is-info is-light" role="alert">
+      <button class="delete"></button>
+      <%= content %>
+    </div>
+    """
+  end
+
+  def flash(content, :error) do
+    ~E"""
+    <div class="notification is-danger is-light" role="alert">
+      <button class="delete"></button>
+      <%= content %>
+    </div>
+    """
+  end
 end
