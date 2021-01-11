add769b7ce6323b6aa5f2a08482a7693baa68e41
Amin Arria <arria.amin@gmail.com>
2021-01-10 23:23:38 -0300

    root page

--- lib/sponsorly_web/controllers/page_controller.ex
+++ lib/sponsorly_web/controllers/page_controller.ex
@@ -2,7 +2,11 @@ defmodule SponsorlyWeb.PageController do
   use SponsorlyWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    case conn.assigns.current_user do
+      nil -> render(conn, "index.html")
+      _user -> redirect(conn, to: Routes.page_path(conn, :dashboard))
+    end
+
   end
 
   def dashboard(conn, _params) do
